474a54742e1cfe8831fe866303e7b4f778c27646
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-09-29 16:33:30 +0200

    Add first servlet that simply says hello

---
+++ src/com/acme/smiley/web/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.acme.smiley.web;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(description = "Greeting Servlet", urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getWriter().println("Hello Smiley World :)");
+	}
+
+}
