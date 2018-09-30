26adcd999f3212785c6ca51c9fdadd14c4c55a2f
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-09-30 19:05:29 +0200

    Apply the MVC architecture to the substitution page
    * For GET request simply forward to a static HTML page.
    * For POST request, the controller manipulate the model, and send it
    back to the viewer via request attributes.

--- src/com/acme/smiley/web/SubstitutionServlet.java
+++ src/com/acme/smiley/web/SubstitutionServlet.java
@@ -1,8 +1,8 @@
 package com.acme.smiley.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,32 +20,24 @@ public class SubstitutionServlet extends HttpServlet {
 	private SubstitutionService service = new SubstitutionService();
 
 	/**
+	 * @throws ServletException 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><title>Substitution Service</title></head><body>");
-		out.println("<form action='#' method='POST'>");
-		out.println("<p>Please enter a message.</p>");
-		out.println("<textarea name='msg'>Hello :)</textarea><p/>");
-		out.println("<button type='submit'>Send</button>");
-		out.println("</form>");
-		out.println("</body></html>");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		request.getRequestDispatcher("/substitution.html").forward(request, response);
 	}
 
 	/**
+	 * @throws ServletException 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		String message = request.getParameter("msg");
 		message = service.substitute(message);
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><title>Substitution Service</title></head><body>");
-		out.println("<p>You entered the following message.</p>");
-		out.printf("<p>%s</p>\n", message);
-		out.println("<a href=''>Try again</a><br/>");
-		out.println("<a href='hello'>Go to welcome page</a><br/>");
-		out.println("</body></html>");
+		request.setAttribute("message", message);
+		request.getRequestDispatcher("/substitution.jsp").forward(request, response);
 	}
 
 }
