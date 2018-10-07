3490e6aa6890ddc3a471e509364cc8add85e0134
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-10-07 18:51:38 +0200

    Towards Web 2: first REST service
    * Use JAX-RS to implement RESTful services.
    * User Jersey, JAX-RS reference implementation.
    * Configure web.xml to expose REST endpoins under /rest.
    * Create first REST service: the substitution service.
    * Use AJAX to call the rest service.

---
+++ src/com/acme/smiley/rest/SubstitutionEndpoint.java
@@ -0,0 +1,21 @@
+package com.acme.smiley.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.acme.smiley.service.SubstitutionService;
+
+@Path("substitute")
+public class SubstitutionEndpoint {
+	
+	private SubstitutionService service = new SubstitutionService();
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String substitute(@QueryParam("msg") String message) {
+		return service.substitute(message);
+	}
+}
